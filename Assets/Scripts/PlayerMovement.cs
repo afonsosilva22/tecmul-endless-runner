4a17eb5b06f3d58e3d9cdd78311d83e4f91f3d85
afonsosilva22 <limasilva.afonso@gmail.com>
2026-03-20 14:19:00 +0000

    add player jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float playerSpeed = 3;
-    public float horizontalSpeed = 5;
+    public float horizontalSpeed = 4;
+    public float jumpForce = 5;
+
+    private Rigidbody rb;
+    private bool isGrounded = true;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -21,6 +25,19 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector3.right * horizontalSpeed * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
 }
