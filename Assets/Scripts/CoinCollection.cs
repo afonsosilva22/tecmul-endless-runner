9658aef5e657fc9513665459bdc1c073bfacbc85
afonsosilva22 <limasilva.afonso@gmail.com>
2026-04-13 14:13:47 +0100

    update coin model + add coin collection script

---
+++ Assets/Scripts/CoinCollection.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CoinCollection : MonoBehaviour
+{
+    public int value = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ScoreManager.instance.AddScore(value);
+            Destroy(gameObject);
+        }
+    }
+}
