b8af96920c26d18d53902edafd7c112d9b06d3c4
afonsosilva22 <limasilva.afonso@gmail.com>
2026-04-20 13:10:33 +0100

    add coin count ui

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,31 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public int score = 0;
+    
+    public TextMeshProUGUI scoreText;
 
     void Awake()
     {
         instance = this;
     }
 
+    void Start()
+    {
+        UpdateScoreUI();
+    }
+
     public void AddScore(int amount)
     {
         score += amount;
-        Debug.Log("Score: " + score);
+        UpdateScoreUI();
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Coins: " + score;
     }
 }
