1fc305ae36256e2b98f40bfa92d5cbfe2a09211c
afonsosilva22 <limasilva.afonso@gmail.com>
2026-04-21 17:27:26 +0100

    add distance tracker

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,11 +6,15 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public int score = 0;
+
+    private Transform player;
+    private float startZ;
     
     public int coinMultiplier = 1;
     public Coroutine multiplierCoroutine;
 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI distanceText;
 
     void Awake()
     {
@@ -20,6 +24,19 @@ public class ScoreManager : MonoBehaviour
     void Start()
     {
         UpdateScoreUI();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        startZ = player.position.z;
+    }
+
+    void Update()
+    {
+        if (player == null)
+        {
+            return;
+        }
+
+        float distance = player.position.z - startZ;
+        distanceText.text = "Distance: " + Mathf.FloorToInt(distance) + "m";
     }
 
     public void AddScore(int amount)
